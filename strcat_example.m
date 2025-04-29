7d9a3d8b62d19fed4efc713b8df2b8e8a5b438c9
Supreeta Vijayakumar <supreetabv@yahoo.co.uk>
2025-04-29 16:16:24 +0100

    Delete strcat_example.m

--- strcat_example.m
+++
@@ -1,7 +0,0 @@
-function strcat_example(x)
-
-bestmatrixfilename = strcat ("outputenz_", x, "_potato_1.txt");
-fprintf ('Filename for best matrix output file will be: %s\n', bestmatrixfilename);
-d_plotfilename = strcat ("d_plot_", x, "_potato_1.xls");
-fprintf ('Filename for d_plot output file will be: %s\n', d_plotfilename);
-
