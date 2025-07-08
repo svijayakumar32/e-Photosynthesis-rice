d0a9de4019dff99a63dfcabddc81fe11ed34fd41
Supreeta Vijayakumar <supreetabv@yahoo.co.uk>
2025-07-08 10:43:08 +0100

    Add files via upload
    Reupload batch job scripts

---
+++ Optimisation/Shell_Scripts/Arrays/job_array_gpmain_rice_280.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -p serial
+#SBATCH --mem=8g
+#SBATCH -J job_array_gpmain_rice_280
+#SBATCH -a 1-15
+
+source /etc/profile
+module add matlab/2022a
+
+matlab -nodisplay -nodesktop -r "gpmain_rice_280_new;quit"
+
+echo This is job task ${SLURM_ARRAY_TASK_ID} running on compute node `uname -n`
